e02d0ad4dbe823d35d69d283f8654890d1a585ee
andyanamafaza <andyanamafaza@gmail.com>
2024-08-20 11:12:24 +0700

    test: modify the testcase

--- test/test-case.js
+++ test/test-case.js
@@ -2,12 +2,13 @@ const axios = require("axios");
 
 const testUrl = "http://localhost:3000/download";
 
-const originalUrls = ["https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf",
-                      "https://www.sharedfilespro.com/shared-files/38/?sample.pdf",
-                      "https://spks.or.id/file/publikasi/Test.pdf"
+const originalUrls = [
+  "https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf",
+  "https://www.sharedfilespro.com/shared-files/38/?sample.pdf",
+  "https://spks.or.id/file/publikasi/Test.pdf"
 ];
 
-const urls = originalUrls.flatMap((url) => Array(5).fill(url));
+const urls = Array.from({ length: 5 }).flatMap(() => originalUrls);
 
 const requestData = { urls };
 
@@ -19,5 +20,7 @@ axios
   })
   .catch((error) => {
     console.error("Error:", error.message);
-    console.error("Response data:", error.response.data);
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+    }
   });

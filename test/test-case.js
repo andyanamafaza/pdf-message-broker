7abdd7651961926d427e1f66793a7d4ebfd66e28
andyanamafaza <andyanamafaza@gmail.com>
2024-08-20 09:37:02 +0700

    feat:add link test case

--- test/test-case.js
+++ test/test-case.js
@@ -2,9 +2,12 @@ const axios = require("axios");
 
 const testUrl = "http://localhost:3000/download";
 
-const originalUrls = ["https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf"];
+const originalUrls = ["https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf",
+                      "https://www.sharedfilespro.com/shared-files/38/?sample.pdf",
+                      "https://spks.or.id/file/publikasi/Test.pdf"
+];
 
-const urls = originalUrls.flatMap((url) => Array(20).fill(url));
+const urls = originalUrls.flatMap((url) => Array(5).fill(url));
 
 const requestData = { urls };
 
